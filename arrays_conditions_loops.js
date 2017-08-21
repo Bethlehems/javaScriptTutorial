baa2ecdbca8ba46f79083b41183c4c943a5c1394
Betty Seifu <betty.seifu149@gmail.com>
2017-08-21 23:05:33 +0300

    conditions and loops

--- arrays_conditions_loops.js
+++ arrays_conditions_loops.js
@@ -2,15 +2,29 @@ var arr=new Array("first",true,20);
 alert(arr.length);
 //or
 var arr2=["first",true,20];
-arr2.push("some new value");//to add new value at the end
+arr2.push("some new value");//adding  new value at the end
 
-var things1=["betty","abebe","bazen"]
-var things2=["hagosa","metta"]
+var things1=["betty","abebe","bazen"],
+ things2=["hagosa","metta"];
 var both_in_one=things1.concat(things2);
-var joined=both_in_one.join(",");
-var reversed=both_in_one.reverse();
-var sorted=both_in_one.sort();
 alert(both_in_one);
+var joined=both_in_one.join(",");
 alert(joined);
-alert(reversed);
-alert(sorted);
+alert(both_in_one.reverse());
+alert(both_in_one.sort());
+//javascript can compare 2 different datatype values by converting both to one same datatype(==)
+//===->identity operator
+if(!("66"!==66)){
+    alert("true");
+}
+else{
+    alert("false");
+}
+
+//falsy conditions=0,null,"",undefined variables
+//truthy condtions=1,any number or letter,[],{}
+
+alert(typeof null);//typeof is used to know datatype of a var
+for(i=0;i<arr2.length;i++){
+    alert(arr2.pop());
+}
