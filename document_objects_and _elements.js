1f3e539d3149da56d53d542526ca81dd6b196c74
Betty Seifu <betty.seifu149@gmail.com>
2017-08-23 15:58:44 +0300

    working document object

--- document_objects_and _elements.js
+++ document_objects_and _elements.js
@@ -1,15 +1,17 @@
 //document object is an object directly beneath the window object
 (function(){
-var pEl=document.getElementById("p");
-// or pEl=document.querySeletorAll("p")
-//alert(pEl).parentNode.tagName-->to know what parent node is 
-for(var i=0;i<pEl.length;i++){
+var pEl=document.getElementsByTagName("p");
+ pEl2=document.querySelector("#foo")
+ alert(pEl2.parentNode.tagName);//-->to know which tag surrounded p tag
+
+for(var i=0,len=pEl.length;i<len;i=i+1){
 alert(pEl[i])
 }
-}());//old way
-//to access the last p with div
 var lastpEl=pEl[pEl.length-1];
 alert(lastpEl);
+}());//old way
+//to access the last p with div
+
 var testVar=document.getElementById("foo");
 // or testpEl=document.querySelector("#foo")
 alert(testVar)
